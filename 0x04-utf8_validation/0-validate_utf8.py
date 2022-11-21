068c799bbd37cf8fb80296af5085f3b12967b50f
horpeazy <iyamuhope.nosa647@gmail.com>
2022-11-21 10:52:21 +0000

    feat: valid utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """ Validate UTF-8 """
 
+
 def validUTF8(data):
-	""" Determines if a given data set represents valid UTF-b Encoding """
-	count = 0
+    """ Determines if a given data set represents valid UTF-b Encoding """
+    count = 0
 
-	for x in data:
-		if 191 >= x >=128:
-			if not count:
-				return False
-		else:
-			if count:
-				return False
-			if x < 128:
-				continue
-			elif x < 224:
-				count = 1
-			elif x < 240:
-				count = 2
-			elif x < 248:
-				count = 3
-			else:
-				return False
+    for x in data:
+        if 191 >= x >= 128:
+            if not count:
+                return False
+        else:
+            if count:
+                return False
+            if x < 128:
+                continue
+            elif x < 224:
+                count = 1
+            elif x < 240:
+                count = 2
+            elif x < 248:
+                count = 3
+            else:
+                return False
 
-	return count == 0
+    return count == 0
